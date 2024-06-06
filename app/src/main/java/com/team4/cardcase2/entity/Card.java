b9e6cd70c5c080291d80f4466ad3ad5193284fcc
wbhpig <wbh123456kevin@126.com>
2024-06-06 12:17:09 +0800

    Change 添加了代码导航逻辑

--- app/src/main/java/com/team4/cardcase2/entity/Card.java
+++ app/src/main/java/com/team4/cardcase2/entity/Card.java
@@ -1,4 +1,50 @@
 package com.team4.cardcase2.entity;
 
 public class Card {
+    private int id;
+    private String email;
+    private String pwd;
+    private String vcode;
+    private int usertype;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getVcode() {
+        return vcode;
+    }
+
+    public void setVcode(String vcode) {
+        this.vcode = vcode;
+    }
+
+    public int getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(int usertype) {
+        this.usertype = usertype;
+    }
+
 }
