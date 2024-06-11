288c7612858d32a5127488b8a8ac99acb4466c03
wbhpig <wbh123456kevin@126.com>
2024-06-11 22:01:42 +0800

    make整合完毕

---
+++ app/src/main/java/com/team4/cardcase2/foreground/Make1Fragment.kt
@@ -0,0 +1,80 @@
+package com.team4.cardcase2.foreground
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.team4.cardcase2.R
+import com.team4.cardcase2.entity.ServerCard
+import com.team4.cardcase2.interfaces.test
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Make1Fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Make1Fragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val root = inflater.inflate(R.layout.fragment_make1, container, false)
+        val myCardView: RecyclerView = root.findViewById(R.id.myCardView)
+        val myCards:List<ServerCard> = test.getMyCard()
+        val adapter = CardAdapter(myCards)
+        myCardView.adapter = adapter
+        myCardView.layoutManager = LinearLayoutManager(requireContext())
+
+        val testButton: Button = root.findViewById(R.id.testButton2)
+        testButton.setOnClickListener {
+            val navController=findNavController()
+            navController.navigate(R.id.make2Fragment)
+        }
+        return root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Make1Fragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Make1Fragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
